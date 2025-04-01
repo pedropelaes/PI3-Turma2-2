1a96dbe4138a8f360c1d6bb214b3d9829536bc46
pedropelaes <pedro.pm2@puccampinas.edu.br>
2025-04-01 14:17:28 -0300

    Telas Iniciais Criação das telas que aparecem ao usuário abrir o app pela primeira vez

--- SuperId_App/app/build.gradle.kts
+++ SuperId_App/app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
@@ -56,4 +57,13 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //acompanist-pager
+    implementation(libs.accompanist.pager)
+    implementation(libs.accompanist.pager.indicators)
+    implementation(libs.accompanist.systemuicontroller)
+    //compose
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.layout)
 }
--- SuperId_App/app/src/main/java/com/example/superid/MainActivity.kt
+++ SuperId_App/app/src/main/java/com/example/superid/MainActivity.kt
@@ -1,47 +1,116 @@
 package com.example.superid
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.superid.ui.theme.SuperIdTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SuperIdTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SuperID()
+        }
+    }
+}
+@Preview
+@Composable
+fun SuperID() {
+    ViewPagerForInitialScreens()
+}
+
+@Composable //Essa função é responsável pelo design das páginas de íniciais
+fun InitialScreensDesign(
+    imageResId: Int,
+    statusBarColor: Color = Color(152034),
+    navigationBarColor: Color = Color(152034),
+    content: @Composable () -> Unit
+) {
+    val systemUiController = rememberSystemUiController()
+    SideEffect { //aplicando as cores da barra de status e navegação
+        systemUiController.setStatusBarColor(statusBarColor, darkIcons = false)
+        systemUiController.setNavigationBarColor(navigationBarColor, darkIcons = false)
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = imageResId),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center,
+        ){
+            content()
+        }
+    }
+
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ViewPagerForInitialScreens() { //view pager das paginas iniciais
+    val pagerState = rememberPagerState(pageCount = { 3 })
+
+    Column {
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.weight(1f)
+        ) { page ->
+            when (page) {
+                0 -> Screen1()
+                1 -> Screen2()
+                2 -> Screen3()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Screen1(){
+    InitialScreensDesign(R.drawable.lockers_background){
+        Text("Bem vindo ao SuperID", color = Color.White)
+    }
 }
-
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    SuperIdTheme {
-        Greeting("Android")
+fun Screen2(){
+    InitialScreensDesign(R.drawable.lockers_background){
+        Text("Termos de condição", color = Color.White)
+    }
+}
+@Composable
+fun Screen3(){
+    InitialScreensDesign(R.drawable.lockers_background){
+        Text("Tela de login", color = Color.White)
     }
 }
