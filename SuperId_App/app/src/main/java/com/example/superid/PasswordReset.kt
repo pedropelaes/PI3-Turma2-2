a9dc73a368c4a50b4b28491c766cc88ae37ac7fa
Mateusmerg <mateus.gm1@puccampinas.edu.br>
2025-04-29 15:24:35 -0300

    ajustes na tela de recuperação de senha

--- SuperId_App/app/src/main/java/com/example/superid/PasswordReset.kt
+++ SuperId_App/app/src/main/java/com/example/superid/PasswordReset.kt
@@ -1,20 +1,23 @@
 package com.example.superid
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,6 +26,8 @@ import com.example.superid.ui.theme.ui.common.SuperIdTitle
 import com.example.superid.ui.theme.ui.common.TextFieldDesignForLoginAndSignUp
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
+
 
 class PasswordReset : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,6 +45,7 @@ fun PasswordResetScreen() {
     var email by remember { mutableStateOf("") }
     var emailSent by remember { mutableStateOf(false) }
     var errorMessage by remember { mutableStateOf("") }
+    val context = LocalContext.current
 
     Column(
         modifier = Modifier
@@ -57,6 +63,21 @@ fun PasswordResetScreen() {
             fontSize = 30.sp,
             color = Color.White,
             fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            "Por favor, insira o email associado à sua conta e nós enviaremos um link de recuperação de senha",
+
+            fontFamily = FontFamily.SansSerif,
+            fontSize = 15.sp,
+            color = Color.White,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp)
         )
 
         Spacer(modifier = Modifier.height(24.dp))
@@ -91,7 +112,7 @@ fun PasswordResetScreen() {
                 .height(45.dp)
                 .width(220.dp)
         ) {
-            Text("Enviar e-mail de recuperação")
+            Text("Enviar e-mail")
         }
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -112,6 +133,28 @@ fun PasswordResetScreen() {
             )
         }
     }
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        TextButton(
+            onClick = {
+                val intent = Intent(context, LogInActivity::class.java)
+                context.startActivity(intent)
+            },
+            modifier = Modifier
+                .padding(8.dp)
+                .align(Alignment.BottomStart)
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = "Voltar",
+                tint = Color.White
+            )
+            Text("Voltar", fontSize = 15.sp, color = Color.White, textDecoration = TextDecoration.Underline)
+        }
+    }
+
+
 }
 
 fun sendPasswordResetEmail(email: String, onResult: (Boolean, String?) -> Unit) {
